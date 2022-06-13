ba13245402a982c2498891001bb9860f59d9ef50
Muzinho862 <105685410+Muzinho862@users.noreply.github.com>
2022-06-13 10:19:43 -0300

    Add files via upload

---
+++ EX006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um Número: '))
+
+print(f'''O dobro de {n} é {n*2}
+o seu triplo é {n*3}
+e a raiz quadrada é de {n**(1/2):.2f}''')
---
+++ EX007.py
@@ -0,0 +1,4 @@
+n = float(input('Digite a 1º nota do aluno: '))
+n2 = float(input('Digite a 2º nota do aluno: '))
+
+print(f'A média aritimética de suas notas é {((n+n2)/2):.1f}')
